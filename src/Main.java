5a45d1de13dde09b7ea001f6876bd787ce77b74a
uthso297 <uthso119464@gmail.com>
2024-08-19 16:24:25 +0600

    Class Methods

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,15 @@ public class Main {
             System.out.println(i);
         }
     }
+    public void publicmethod(){
+        System.out.println("This is public method");
+    }
+    public void car(){
+        System.out.println("The car is going as fast as it can");
+    }
+    public void speed(int maxspeed){
+            System.out.println("Max speed is " +maxspeed);
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         Main myObj1 = new Main();
@@ -14,5 +23,10 @@ public class Main {
         Main myObj2 = new Main();
         myObj2.y = 20;
         System.out.println(myObj2.y);
+        Main myObj3 = new Main();
+        myObj3.publicmethod();
+        Main myCar = new Main();
+        myCar.car();
+        myCar.speed(59);
     }
 }
